4b740a414f30f070ef8beb0ca3d106bcbcea5cee
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-06-06 23:20:27 +0900

    SearchLog

--- src/main/java/com/usopenblog/usopen_blog_backend_20240515/entity/SearchLogEntity.java
+++ src/main/java/com/usopenblog/usopen_blog_backend_20240515/entity/SearchLogEntity.java
@@ -1,4 +1,25 @@
 package com.usopenblog.usopen_blog_backend_20240515.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="search-log")
+@Table(name="search-log")
 public class SearchLogEntity {
+
+  @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private int sequnce;
+
+  private String searchWord;
+
+  private String relationWorld;
+
+  private boolean relation;
 }
