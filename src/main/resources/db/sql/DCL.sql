474e2b177e26202aa3c1e8414510dce6d76a3c6d
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-06-07 00:55:12 +0900

    db

---
+++ src/main/resources/db/sql/DCL.sql
@@ -0,0 +1,13 @@
+create user 'usopen'@'*' IDENTIFIED BY '1234';
+
+       use busan;
+
+       CREATE USER 'usopen'@'%' IDENTIFIED BY '1234';
+SELECT User, Host FROM mysql.user WHERE User = 'usopen';
+DROP USER 'usopen'@'%';
+CREATE USER 'usopen'@'%' IDENTIFIED BY '1234';
+
+
+grant select, update, delete, insert
+on busan.*
+TO 'usopen'@'*';
