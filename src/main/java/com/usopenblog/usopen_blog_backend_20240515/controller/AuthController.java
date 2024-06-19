714171c83e340229fd24a6e946a22fc1066424a0
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-06-19 22:21:14 +0900

    Commit

--- src/main/java/com/usopenblog/usopen_blog_backend_20240515/controller/AuthController.java
+++ src/main/java/com/usopenblog/usopen_blog_backend_20240515/controller/AuthController.java
@@ -10,7 +10,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+//@ResponseBody가 추가 되었고, json 형태로 객체를 데이터를 반환
 @RestController
+//
 @RequestMapping("/api/v1/auth")
 @RequiredArgsConstructor
 public class AuthController {
