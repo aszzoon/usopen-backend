438e166bbc14735238ea3489f70df29b43d904dd
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-07-09 21:03:37 +0900

    GetBoardResponseDto

--- src/main/java/com/usopen/backend/dto/response/board/GetBoardResponseDto.java
+++ src/main/java/com/usopen/backend/dto/response/board/GetBoardResponseDto.java
@@ -1,8 +1,11 @@
 package com.usopen.backend.dto.response.board;
 
 import com.usopen.backend.common.ResponseCode;
+import com.usopen.backend.common.ResponseMessage;
 import com.usopen.backend.dto.response.ResponseDto;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 import java.util.List;
 
@@ -13,6 +16,7 @@ public class GetBoardResponseDto extends ResponseDto {
   private String title;
   private String content;
   private List<String> boardImagesList;
+  private String writeDatetime;
   private String writerEmail;
   private String writerNickname;
   private String writerProfileImage;
@@ -21,4 +25,12 @@ public class GetBoardResponseDto extends ResponseDto {
     super(ResponseCode.SUCCESS, ResponseCode.SUCCESS);
   }
 
+  public static ResponseEntity<GetBoardResponseDto> success() {
+    GetBoardResponseDto result = new GetBoardResponseDto();
+    return ResponseEntity.status(HttpStatus.OK).body(result);
+  }
+
+  public static ResponseEntity<ResponseDto> notExistBoard() {
+    ResponseDto result = new ResponseDto(ResponseCode.NOT_EXISTED_BOARD, ResponseMessage.NOT_EXISTED_BOARD); return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(result);
+  }
 }
