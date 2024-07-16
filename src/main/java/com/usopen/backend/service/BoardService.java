decb368080e15f64a1c5c41c0ff2945df29abb9e
AHN,SEONG-JOON <84303387+aszzoon@users.noreply.github.com>
2024-07-16 22:12:26 +0900

    commit

--- src/main/java/com/usopen/backend/service/BoardService.java
+++ src/main/java/com/usopen/backend/service/BoardService.java
@@ -1,10 +1,11 @@
 package com.usopen.backend.service;
 
 import com.usopen.backend.dto.request.board.PostBoardRequestDto;
+import com.usopen.backend.dto.response.board.GetBoardResponseDto;
 import com.usopen.backend.dto.response.board.PostBoardResponseDto;
 import org.springframework.http.ResponseEntity;
 
 public interface BoardService {
-
+  ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber);
   ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email);
 }
--- src/main/java/com/usopen/backend/service/implement/BoardServiceImplement.java
+++ src/main/java/com/usopen/backend/service/implement/BoardServiceImplement.java
@@ -2,8 +2,11 @@ package com.usopen.backend.service.implement;
 
 import com.usopen.backend.dto.request.board.PostBoardRequestDto;
 import com.usopen.backend.dto.response.ResponseDto;
+import com.usopen.backend.dto.response.board.GetBoardResponseDto;
 import com.usopen.backend.dto.response.board.PostBoardResponseDto;
 import com.usopen.backend.repository.BoardRepository;
+import com.usopen.backend.repository.ImageRepository;
+import com.usopen.backend.repository.UserRepository;
 import com.usopen.backend.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -14,6 +17,8 @@ import org.springframework.stereotype.Service;
 public class BoardServiceImplement implements BoardService {
 
   private final BoardRepository boardRepository;
+  private final ImageRepository imageRepository;
+  private final UserRepository userRepository;
 
   @Override
   public ResponseEntity<? super PostBoardResponseDto> postBoard(PostBoardRequestDto dto, String email) {
@@ -21,12 +26,16 @@ public class BoardServiceImplement implements BoardService {
     try {
 
 
-
     } catch (Exception exception) {
       exception.printStackTrace();
       return ResponseDto.databaseError();
     }
-
     return PostBoardResponseDto.success();
   }
+
+  @Override
+  public ResponseEntity<? super GetBoardResponseDto> getBoard(Integer boardNumber) {
+    return null;
+  }
+
 }
